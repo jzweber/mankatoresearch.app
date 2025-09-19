e9fb286c022c73ecabf9542e6e3cb3195e6d37c4
JZweber <wb2584nl@go.minnstate.edu>
2025-09-19 13:24:49 -0500

    fix yaml file deleted creds

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - .swa (Azure Static Web Apps)
-     */
-    '/((?!.swa).*)',
-  ],
-}
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   // Where Next.js puts the build output
-  distDir: 'build',
-
   // Enable standalone build for easier deployment
   output: 'standalone',
 };
